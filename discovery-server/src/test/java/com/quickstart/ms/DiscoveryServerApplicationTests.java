1247fcec8f24efcd0785afd1749c93695291552c
prithvi <kumarakshay895@gmail.com>
2021-01-02 17:06:26 +0530

    first commit

---
+++ discovery-server/src/test/java/com/quickstart/ms/DiscoveryServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.quickstart.ms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
